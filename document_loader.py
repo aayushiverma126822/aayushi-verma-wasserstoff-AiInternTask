1d390f80248117d288bd71a8b86f4c1f1c976b13
Aayushi verma <143534982+aayushiverma126822@users.noreply.github.com>
2025-07-01 04:08:41 +0530

    Update document_loader.py

--- document_loader.py
+++ document_loader.py
@@ -1,5 +1,13 @@
 import os
-from langchain.document_loaders import PyPDFLoader, TextLoader, UnstructuredFileLoader
+import docx
+from langchain.schema import Document
+from langchain.document_loaders import PyPDFLoader, TextLoader
+
+def read_docx(file):
+    doc = docx.Document(file)
+    text = "\n".join([para.text for para in doc.paragraphs])
+    metadata = {"source": file.name}
+    return [Document(page_content=text, metadata=metadata)]
 
 def load_documents(uploaded_files):
     documents = []
@@ -7,11 +15,12 @@ def load_documents(uploaded_files):
         file_name = file.name
         if file_name.endswith(".pdf"):
             loader = PyPDFLoader(file)
+            documents.extend(loader.load())
         elif file_name.endswith(".txt"):
             loader = TextLoader(file)
+            documents.extend(loader.load())
         elif file_name.endswith(".docx"):
-            loader = UnstructuredFileLoader(file)
+            documents.extend(read_docx(file))
         else:
             continue
-        documents.extend(loader.load())
     return documents
