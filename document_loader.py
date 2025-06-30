c443cf335e3fb95fa08c5295ddffc30f3dd3a92f
aayushiverma126822 <143534982+aayushiverma126822@users.noreply.github.com>
2025-07-01 02:13:28 +0530

    Create document_loader.py

---
+++ document_loader.py
@@ -0,0 +1,17 @@
+import os
+from langchain.document_loaders import PyPDFLoader, TextLoader, Docx2txtLoader
+
+def load_documents(uploaded_files):
+    documents = []
+    for file in uploaded_files:
+        file_name = file.name
+        if file_name.endswith(".pdf"):
+            loader = PyPDFLoader(file)
+        elif file_name.endswith(".txt"):
+            loader = TextLoader(file)
+        elif file_name.endswith(".docx"):
+            loader = Docx2txtLoader(file)
+        else:
+            continue
+        documents.extend(loader.load())
+    return documents
